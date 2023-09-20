6f0bb8f1f999156a360ff8673e01918bcd452380
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-19 23:39:00 -0700

    set up basic timestampRouter.js

---
+++ routes/timestampRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const timestampController = require('../controllers/timestampController');
+
+const router = express.Router();
+
+// router.post('/api/timestamp/:date?')
+
+// module.exports router;
