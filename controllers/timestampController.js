675286583a3fd6ea414763d53db96b08f03f3b65
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-21 22:09:46 -0700

    Handle an empty input for date

--- controllers/timestampController.js
+++ controllers/timestampController.js
@@ -12,6 +12,14 @@ If empty then send the current time with a unix key
 
 exports.getTimeStamp = (req, res) => {
   let date = req.params.date;
+  console.log(date)
+  
+  // handle an empty input
+  if(date === undefined || date.length < 1) {
+    let currentDate = new Date();
+    let utcFormat = currentDate.toUTCString();
+    return res.json({unix: currentDate.getTime(), utc: utcFormat});
+  } else {
 
-  res.status(200).json({ message: date });
+  }
 };
