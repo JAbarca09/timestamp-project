3c73b3d0bfe78fcd8b38fa3722e19dfc2b9e6406
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-21 21:43:03 -0700

    Add psuedo code notes in timestampController.js

--- controllers/timestampController.js
+++ controllers/timestampController.js
@@ -1,5 +1,15 @@
 // endpoints logic go here
 
+/*
+Logic notes:
+Check if the date is in the YYYY-MM-DD format
+  return a date following this format {"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+Check if the date is in the UNIX format
+  return a date in this format {"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+If not either then send invalid date
+If empty then send the current time with a unix key
+*/
+
 exports.getTimeStamp = (req, res) => {
   let date = req.params.date;
 
