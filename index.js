7c6c32bba6c9f4a0ae85b15940bd236d753838a7
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-22 22:35:44 -0700

    Fix the api route to /api rather than api/timestamp

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.use('/api/timestamp', timestampController);
+app.use('/api', timestampController);
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
